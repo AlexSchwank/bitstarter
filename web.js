856e6a73ff10db7a76b444f34621ff7a4fae326f
AlexSchwank <alschwank@gmail.com>
2013-07-09 20:59:57 +0200

    updated reading file

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+	var string = fs.readFileSync('./index.html');
 	buf = new Buffer(256);
-	len = buf.write(fs.readFileSync('./index.html'), 0);
+	len = buf.write(string, 0);
 	console.log(len + " bytes: " + buf.toString('utf8', 0, len));
   	response.send(buf.toString('utf8', 0, len));
 });
